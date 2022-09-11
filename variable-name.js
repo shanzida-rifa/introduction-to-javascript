d8c75f5309ec60a5d4e2640f3d109717e27d0d1c
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-11 11:17:45 +0600

    notes

---
+++ variable-name.js
@@ -0,0 +1,20 @@
+var price = 29;
+// 1. variable name can not be any reserve/key word
+var false = 96;
+var return = 56;
+// 2. variable name has to be in one work. No space
+var my home address = "new california";
+
+// 3. variable name can not have quotation
+var "name" = "Tom Hanks";
+// 4.cannot starts with a number but can ends with a number
+var 77bjhs = kjf;
+var jhf556 = 879;
+
+// 5. How to use long names
+// can not use dash but can use underscore
+var user-name ="raj bro";
+var usercurrenthomeaddress = "andor killa bandban";
+var user_address = "andor killa bandban";
+var userAddress = "andor killa bandban";
+// case sensitive
--- variable.js
+++ variable.js
@@ -1,4 +1,14 @@
-// vary+able=variabl
+// vary+able=variable
+
+//numeric
 var price = 11;
 var age = 23;
 var temperature = 38;
+
+//string
+var person = "sodor - uddin";
+var location = "andor killa bandorban";
+
+//Boolean
+var serious = true;
+var isSingle = false;
