580f8f07d51319243b8eff2c7ca263486b2723da
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-11 16:13:27 +0600

    home-work

---
+++ home-work.js
@@ -0,0 +1,32 @@
+//Practice Problem 1
+var momGave = 1000;
+var orangePrice = 300;
+var applePrice = 400;
+var totalCost = orangePrice + applePrice;
+var returnPrice = momGave - totalCost;
+console.log(returnPrice);
+
+// Practice Problem 2
+
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bang = 99.50;
+
+var totalMark = math + bio + chem + phy + bang;
+var avgMark = totalMark / 5;
+avgMark = avgMark.toFixed(2);
+avgMark = parseFloat(avgMark);
+console.log(avgMark);
+
+// Practice Problem 3
+var comp1 = "You are going to be an"
+var comp2 = "awesome web developer."
+var compliment = comp1 + '' + comp2;
+console.log(compliment);
+
+// Practice Problem 4
+var gaveNum = 119;
+var mod = gaveNum % 5;
+console.log(mod);
