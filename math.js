eeccf43cdb189b33eb954cecc98c4089ab923b1b
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-11 10:45:07 +0600

    added

---
+++ math.js
@@ -0,0 +1,17 @@
+/*
+ 1.number==> 55,897, -32
+ 2. integer,odd,even: 2,5
+ 3. float decimal: 2.5, 13.67
+ 4. Operation: addition,substraction,remainder
+ 5. percent,
+ 6.average
+ 7. comparison: >,<,==,!=,
+ 8. Oikik:
+ 9. max-number,min,sum,avg
+10. series: fibacci
+Extra:::
+1.prime
+2.permutation
+3.:::ALWAYS:::
+jodi lage..
+*/
---
+++ variable.js
@@ -0,0 +1,4 @@
+// vary+able=variabl
+var price = 11;
+var age = 23;
+var temperature = 38;
